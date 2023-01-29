434ae81cf7e07b88e01b723f3afa18e931be536c
SwapnaliKhade0608 <123722688+SwapnaliKhade0608@users.noreply.github.com>
2023-01-29 19:23:53 +0530

    Demo project

---
+++ Demo/src/demopackage/Github.java
@@ -0,0 +1,12 @@
+package demopackage;
+
+public class Github {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Github");
+		System.out.println("New repo is created");
+		System.out.println("we are in new repo");
+		System.out.println("Work is finished");
+	}
+
+}
